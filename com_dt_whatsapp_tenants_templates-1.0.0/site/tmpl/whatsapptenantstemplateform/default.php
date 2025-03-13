412f6a77b88a3862459eacaf7803b7a6199de499
Naufal Hardiansyah <naufal.hardiansyah@binus.edu>
2025-03-14 02:47:04 +0700

    adj: improve code formatting and add client-side validation message for template name

--- com_dt_whatsapp_tenants_templates-1.0.0/site/tmpl/whatsapptenantstemplateform/default.php
+++ com_dt_whatsapp_tenants_templates-1.0.0/site/tmpl/whatsapptenantstemplateform/default.php
@@ -25,7 +25,7 @@ HTMLHelper::_('bootstrap.tooltip');
 $lang = Factory::getLanguage();
 $lang->load('com_dt_whatsapp_tenants_templates', JPATH_SITE);
 
-$user    = Factory::getApplication()->getIdentity();
+$user = Factory::getApplication()->getIdentity();
 $canEdit = Dt_whatsapp_tenants_templatesHelper::canUserEdit($this->item, $user);
 
 
@@ -33,16 +33,16 @@ $canEdit = Dt_whatsapp_tenants_templatesHelper::canUserEdit($this->item, $user);
 
 <div class="whatsapptenantstemplate-edit front-end-edit">
 
-<?php if ($this->params->get('show_page_heading')) : ?>
-    <div class="page-header">
-        <h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
-    </div>
-    <?php endif;?>
-	<?php if (!$canEdit) : ?>
+	<?php if ($this->params->get('show_page_heading')): ?>
+		<div class="page-header">
+			<h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
+		</div>
+	<?php endif; ?>
+	<?php if (!$canEdit): ?>
 		<h3>
-		<?php throw new \Exception(Text::_('COM_DT_WHATSAPP_TENANTS_TEMPLATES_ERROR_MESSAGE_NOT_AUTHORISED'), 403); ?>
+			<?php throw new \Exception(Text::_('COM_DT_WHATSAPP_TENANTS_TEMPLATES_ERROR_MESSAGE_NOT_AUTHORISED'), 403); ?>
 		</h3>
-	<?php else : ?>
+	<?php else: ?>
 		<?php if (!empty($this->item->id)): ?>
 			<h1><?php echo Text::sprintf('COM_DT_WHATSAPP_TENANTS_TEMPLATES_EDIT_ITEM_TITLE', $this->item->id); ?></h1>
 		<?php else: ?>
@@ -50,44 +50,48 @@ $canEdit = Dt_whatsapp_tenants_templatesHelper::canUserEdit($this->item, $user);
 		<?php endif; ?>
 
 		<form id="form-whatsapptenantstemplate"
-			  action="<?php echo Route::_('index.php?option=com_dt_whatsapp_tenants_templates&task=whatsapptenantstemplateform.save'); ?>"
-			  method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
-			
-	<input type="hidden" name="jform[id]" value="<?php echo isset($this->item->id) ? $this->item->id : ''; ?>" />
+			action="<?php echo Route::_('index.php?option=com_dt_whatsapp_tenants_templates&task=whatsapptenantstemplateform.save'); ?>"
+			method="post" class="form-validate form-horizontal" enctype="multipart/form-data">
 
-	<input type="hidden" name="jform[state]" value="<?php echo isset($this->item->state) ? $this->item->state : ''; ?>" />
+			<input type="hidden" name="jform[id]" value="<?php echo isset($this->item->id) ? $this->item->id : ''; ?>" />
 
-				<?php echo $this->form->getInput('created_by'); ?>
-				<?php echo $this->form->getInput('modified_by'); ?>
-	<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'whatsapptenantstemplate')); ?>
-	<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'whatsapptenantstemplate', Text::_('COM_DT_WHATSAPP_TENANTS_TEMPLATES_TAB_WHATSAPPTENANTSTEMPLATE', true)); ?>
-	<?php echo $this->form->renderField('name'); ?>
+			<input type="hidden" name="jform[state]"
+				value="<?php echo isset($this->item->state) ? $this->item->state : ''; ?>" />
 
-	<?php echo $this->form->renderField('language'); ?>
+			<?php echo $this->form->getInput('created_by'); ?>
+			<?php echo $this->form->getInput('modified_by'); ?>
+			<?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', array('active' => 'whatsapptenantstemplate')); ?>
+			<?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'whatsapptenantstemplate', Text::_('COM_DT_WHATSAPP_TENANTS_TEMPLATES_TAB_WHATSAPPTENANTSTEMPLATE', true)); ?>
+			<?php echo $this->form->renderField('name'); ?>
 
-	<?php echo $this->form->renderField('category'); ?>
+			<?php echo $this->form->renderField('language'); ?>
 
-	<?php echo $this->form->renderField('header_type'); ?>
+			<?php echo $this->form->renderField('category'); ?>
 
-	<?php echo $this->form->renderField('header_text'); ?>
+			<?php echo $this->form->renderField('header_type'); ?>
 
-	<?php echo $this->form->renderField('header_media'); ?>
+			<?php echo $this->form->renderField('header_text'); ?>
 
-				<?php if (!empty($this->item->header_media)) : ?>
-					<?php $header_mediaFiles = array(); ?>
-					<?php foreach ((array)$this->item->header_media as $fileSingle) : ?>
-						<?php if (!is_array($fileSingle)) : ?>
-							<a href="<?php echo Route::_(Uri::root() . 'uploads' . DIRECTORY_SEPARATOR . Factory::getApplication()->getIdentity()->id . DIRECTORY_SEPARATOR . $fileSingle, false);?>"><?php echo $fileSingle; ?></a> | 
-							<?php $header_mediaFiles[] = $fileSingle; ?>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				<input type="hidden" name="jform[header_media_hidden]" id="jform_header_media_hidden" value="<?php echo implode(',', $header_mediaFiles); ?>" />
-				<?php endif; ?>
-	<?php echo $this->form->renderField('body'); ?>
+			<?php echo $this->form->renderField('header_media'); ?>
 
-	<?php echo $this->form->renderField('footer'); ?>
+			<?php if (!empty($this->item->header_media)): ?>
+				<?php $header_mediaFiles = array(); ?>
+				<?php foreach ((array) $this->item->header_media as $fileSingle): ?>
+					<?php if (!is_array($fileSingle)): ?>
+						<a
+							href="<?php echo Route::_(Uri::root() . 'uploads' . DIRECTORY_SEPARATOR . Factory::getApplication()->getIdentity()->id . DIRECTORY_SEPARATOR . $fileSingle, false); ?>"><?php echo $fileSingle; ?></a>
+						|
+						<?php $header_mediaFiles[] = $fileSingle; ?>
+					<?php endif; ?>
+				<?php endforeach; ?>
+				<input type="hidden" name="jform[header_media_hidden]" id="jform_header_media_hidden"
+					value="<?php echo implode(',', $header_mediaFiles); ?>" />
+			<?php endif; ?>
+			<?php echo $this->form->renderField('body'); ?>
 
-	<?php echo HTMLHelper::_('uitab.endTab'); ?>
+			<?php echo $this->form->renderField('footer'); ?>
+
+			<?php echo HTMLHelper::_('uitab.endTab'); ?>
 			<div class="control-group">
 				<div class="controls">
 
@@ -98,18 +102,23 @@ $canEdit = Dt_whatsapp_tenants_templatesHelper::canUserEdit($this->item, $user);
 						</button>
 					<?php endif; ?>
 					<a class="btn btn-danger"
-					   href="<?php echo Route::_('index.php?option=com_dt_whatsapp_tenants_templates&task=whatsapptenantstemplateform.cancel'); ?>"
-					   title="<?php echo Text::_('JCANCEL'); ?>">
-					   <span class="fas fa-times" aria-hidden="true"></span>
+						href="<?php echo Route::_('index.php?option=com_dt_whatsapp_tenants_templates&task=whatsapptenantstemplateform.cancel'); ?>"
+						title="<?php echo Text::_('JCANCEL'); ?>">
+						<span class="fas fa-times" aria-hidden="true"></span>
 						<?php echo Text::_('JCANCEL'); ?>
 					</a>
 				</div>
 			</div>
 
-			<input type="hidden" name="option" value="com_dt_whatsapp_tenants_templates"/>
-			<input type="hidden" name="task"
-				   value="whatsapptenantstemplateform.save"/>
+			<input type="hidden" name="option" value="com_dt_whatsapp_tenants_templates" />
+			<input type="hidden" name="task" value="whatsapptenantstemplateform.save" />
 			<?php echo HTMLHelper::_('form.token'); ?>
 		</form>
 	<?php endif; ?>
 </div>
+
+<script>
+	$(document).ready(function () {
+		$('#jform_name').after('<div class="name_template_info">The message template name can only have lower-case letters and underscores.</div>');
+	});
+</script>
